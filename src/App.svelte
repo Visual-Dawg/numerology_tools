4c9ca444366e3e5939a57207c7a866fdcefa8a2e
Christofer Koeth <imchris@tuta.io>
2023-01-02 22:02:45 +0200

    prepare routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+  import { Router, Route } from "svelte-navigator"
   import PersonalNumbers from "./components/pages/Personal_Numbers.svelte"
   import Navbar from "./organisms/Navbar.svelte"
 </script>
 
 <main class="mx-6">
-  <Navbar />
-
-  <PersonalNumbers />
+  <Router>
+    <Navbar />
+    <div class="max-w-[1600px] mx-auto">
+      <Route path="/">
+        <PersonalNumbers />
+      </Route>
+    </div>
+  </Router>
 </main>
