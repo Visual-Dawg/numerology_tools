6e89050ed1546e98fd6f490a72b507e22c4ee4a8
Christofer Koeth <imchris@tuta.io>
2023-02-02 18:56:58 +0100

    redesign navbar

--- src/organisms/Navbar.svelte
+++ src/organisms/Navbar.svelte
@@ -4,26 +4,30 @@
 </script>
 
 <nav
-  class="container fixed left-1/2 top-2 flex -translate-x-1/2 items-center justify-between py-2 px-6"
+  class="fixed inset-0 flex h-[128px] w-screen items-center justify-center py-2 px-6"
 >
-  <div class="text-sm md:text-lg">
-    <Link to="/"
-      ><div class="flex items-center">
-        <img
-          src="numerology-toolbelt.svg"
-          class="h-8 w-8"
-          alt="Numerology Toolbelt Logo"
-        /> Numerology Toolbelt
-      </div></Link
-    >
-  </div>
+  <div
+    class="flex w-[748px] items-center justify-between rounded-full bg-amber-50 px-6 py-2 shadow"
+  >
+    <div class="text-sm md:text-lg">
+      <Link to="/"
+        ><div class="flex items-center">
+          <img
+            src="numerology-toolbelt.svg"
+            class="h-8 w-8"
+            alt="Numerology Toolbelt Logo"
+          /> Numerology Toolbelt
+        </div></Link
+      >
+    </div>
 
-  <ul class="flex gap-4 text-sm font-bold md:flex-row">
-    <li class="hover:text-orange-700">
-      <Link to="/">Numbers Calculation</Link>
-    </li>
-    <li class="hover:text-orange-700">
-      <Link to={ROUTES.dateFinder}>Date Finder</Link>
-    </li>
-  </ul>
+    <ul class="flex gap-4 text-sm font-bold md:flex-row">
+      <li class="hover:text-orange-700">
+        <Link to="/">Numbers Calculation</Link>
+      </li>
+      <li class="hover:text-orange-700">
+        <Link to={ROUTES.dateFinder}>Date Finder</Link>
+      </li>
+    </ul>
+  </div>
 </nav>
